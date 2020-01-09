32173e397b1eb857e392036537860866fafc410c
mbouhda <bouhda.mohamed@gmail.com>
2020-01-09 16:48:48 +0100

    Spring cloud config client 1

---
+++ config-client1/src/main/java/com/mbouhda/springcloudconfigclient/tools/RepoProperties.java
@@ -0,0 +1,17 @@
+package com.mbouhda.springcloudconfigclient.tools;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Component
+public class RepoProperties {
+
+    @Value("${property-from-repo}")
+    private String propertyValue;
+
+}
